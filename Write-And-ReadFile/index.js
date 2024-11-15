fb8c219d2147632bd9f0237a8b96ac405fb2f31b
Kumari S <118825518+Kumari-shivaswamy@users.noreply.github.com>
2024-11-15 10:27:11 +0530

    reading file node module

--- Write-And-ReadFile/index.js
+++ Write-And-ReadFile/index.js
@@ -4,3 +4,7 @@ fs.writeFile("message.txt","Hello from Kumari on learning NOde ",(err) => {
     if (err) throw err;
     console.log('writing the file    and  The file has been saved!');
   }); 
+fs.readFile("./message.txt", 'utf8',(err, data) => {
+  if (err) throw err;
+  console.log("reading the file"+data);
+}); 
