6df769e3b9fb347db13381c339deedd9403a83a4
Kumari S <118825518+Kumari-shivaswamy@users.noreply.github.com>
2024-11-15 10:24:37 +0530

    writing file

--- Write-And-ReadFile/index.js
+++ Write-And-ReadFile/index.js
@@ -1,1 +1,6 @@
+const fs  = require("fs");
 
+fs.writeFile("message.txt","Hello from Kumari on learning NOde ",(err) => {
+    if (err) throw err;
+    console.log('writing the file    and  The file has been saved!');
+  }); 
